0f4b97efa058c02c536a95734a38d0f3706d81d0
Dan Simonson <dan.simonson@gmail.com>
2016-10-16 15:09:07 -0400

    Formalized backups and tweets per file

--- spyts.py
+++ spyts.py
@@ -2,7 +2,7 @@
 # SPyTS: The World's Worst Python-based Twitter Scraper #
 #########################################################
 #
-# Dan Simonson, 2015
+# Dan Simonson, 2015, 2016
 
 from pprint import pprint
 import patience
@@ -26,11 +26,35 @@ def cycle(string):
     data = the_signal.clean_data(get(string))
     return data
 
+
+
+def output_data(stuff):
+    "Dump the data appropriately."
+    
+    # pickle is always output as a backup
+    session_id = str(datetime.datetime.now()).replace(" ",".")
+    dump(tweets, open("OUTPUT_TWEETS_%s.pkl"%session_id,"wb"))
+
+    if config["output"] == "json":
+        output.json(tweets, session_id)
+    elif config["output"] == "csv":
+        output.csv(tweets, session_id) 
+    elif config["output"] == "pickle":
+        pass
+    else: 
+        print "Probably should have told you this before, but your output"
+        print "format is unrecognized. Nevertheless, a serialized python pickle"
+        print "has been dumped in its stead."
+        print
+
 queries = config["queries"]
 tweets = []
 ids = set()
+cycle_counter = 0
 while True:
     pause = patience.in_minutes(len(queries))
+    print "Cycle",cycle_counter,"initiated."
+    print "Duration to next cycle:",str(pause)
     try:
         #retrieve
         new_tweets = sum(map(cycle, queries), [])
@@ -43,11 +67,11 @@ while True:
         ids |= set(new_ids)
         tweets += new_tweets
 
-        print "Cycle complete."
+        print "Cycle",cycle_counter,"complete."
         print "Total found ids: "+str(len(tweets))
         print "new ids: "+str(len(new_tweets))
         print "Next cycle at "+str(pause)
-        
+
     except Exception as e:
         print "Exception raised during request."
         print e
@@ -58,31 +82,30 @@ while True:
     print
     print
     
-    if len(tweets) > config["maximum"]: break
-    
-    print "Dumping backup... DO NOT BREAK RIGHT NOW."
-    dump(tweets, open("BACKUP_TWEETS.pkl", "wb"))
-    print "Backup complete. See cycle info above."
-    print "You may safely break right now, until the next cycle begins."
+    if config["maximum"] > 0 and len(tweets) > config["maximum"]: break
 
+    if "backup" in config and cycle_counter % config["backup"] == 0: 
+        print "Dumping backup... DO NOT BREAK RIGHT NOW."
+        dump(tweets, open("BACKUP_TWEETS.pkl", "wb"))
+        print "Backup complete. See cycle info above."
+        print
+    
+    if "tweets_per_file" in config and len(tweets) > config["tweets_per_file"]:
+        print "Chunk complete."
+        output_data(tweets)
+        tweets = []
+
+    
+    print "You may safely break right now, until the next cycle begins."
+    print "*"*50
+    print
+
+    cycle_counter += 1
     try: patience.wait(pause)
     except KeyboardInterrupt: break #safe escape with break
     print "Running..."
 
-#output data
-# always dump the pickle, as a backup
-#pprint(tweets)
-session_id = str(datetime.datetime.now()).replace(" ",".")
-dump(tweets, open("OUTPUT_TWEETS_%s.pkl"%session_id,"wb"))
+print "Terminal dump initiated."
+output_data(tweets)
+print "Complete."
 
-if config["output"] == "json":
-    output.json(tweets, session_id)
-elif config["output"] == "csv":
-    output.csv(tweets, session_id) 
-elif config["output"] == "pickle":
-    pass
-else: 
-    print "Probably should have told you this before, but your output"
-    print "format is unrecognized. I'm just gonna dump the output as"
-    print "json."
-    print
